60b8faa5b8f965e8d36945bd066b1b9e9acb8439
unknown <edgarmaldonado391@ujgh.edu.ve>
2021-11-19 18:30:23 -0400

    Bucle Do While

---
+++ Bucle Do While 2/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 1;
+    do
+    {
+
+    cout << "i ahora es: " << i << endl;
+    i = i + 1;
+    } while (i < 8);
+
+
+    return 0;
+}
